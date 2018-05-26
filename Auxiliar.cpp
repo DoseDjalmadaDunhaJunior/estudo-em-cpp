162130b36782165d27d45b3429ee8f30b56f59d3
djalma <djalminhajr_cunha@hotmail.com>
2018-05-26 15:06:28 -0300

    nao tem nada, ate entao foi simplismente para gerar um caminho ao github

---
+++ Auxiliar.cpp
@@ -0,0 +1,5 @@
+//
+// Created by djalma cunha on 26/05/2018.
+//
+
+#include "Auxiliar.h"
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!" <<endl;
+    cout<<"to com sono"<<endl;
+    return 0;
+}
